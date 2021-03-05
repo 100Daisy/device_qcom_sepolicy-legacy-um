8d16189e86a8af0b5ff0023c7c14e8e803f25ff0
100Daisy <48625180+100Daisy@users.noreply.github.com>
2021-03-05 13:06:40 +0100

    do not override current sepolicy-legacy-um-folder

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,14 +1,14 @@
 # Board specific SELinux policy variable definitions
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy-um/vendor/common \
-    device/qcom/sepolicy-legacy-um/vendor/ssg \
-    device/qcom/sepolicy-legacy-um/timeservice \
-    device/qcom/sepolicy-legacy-um/vendor/common/sysmonapp
+    device/qcom/sepolicy-legacy-um2/vendor/common \
+    device/qcom/sepolicy-legacy-um2/vendor/ssg \
+    device/qcom/sepolicy-legacy-um2/timeservice \
+    device/qcom/sepolicy-legacy-um2/vendor/common/sysmonapp
 
 ifeq ($(TARGET_SEPOLICY_DIR),)
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-legacy-um/vendor/$(TARGET_BOARD_PLATFORM)
+BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-legacy-um2/vendor/$(TARGET_BOARD_PLATFORM)
 else
-BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-legacy-um/vendor/$(TARGET_SEPOLICY_DIR)
+BOARD_SEPOLICY_DIRS += device/qcom/sepolicy-legacy-um2/vendor/$(TARGET_SEPOLICY_DIR)
 endif
 
 ifneq (,$(filter userdebug eng, $(TARGET_BUILD_VARIANT)))
@@ -18,13 +18,13 @@ endif
 
 ifneq (,$(filter 24 25 26 27, $(PRODUCT_SHIPPING_API_LEVEL)))
 BOARD_SEPOLICY_DIRS += \
-    device/qcom/sepolicy-legacy-um/vendor/ota/$(TARGET_BOARD_PLATFORM)
+    device/qcom/sepolicy-legacy-um2/vendor/ota/$(TARGET_BOARD_PLATFORM)
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/qcom/sepolicy-legacy-um/public
+    device/qcom/sepolicy-legacy-um2/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/qcom/sepolicy-legacy-um/private
+    device/qcom/sepolicy-legacy-um2/private
 
 -include device/octavi/sepolicy/qcom/sepolicy.mk
